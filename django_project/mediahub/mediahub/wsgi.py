c774c7063e00210b2a9cef13aeed292300ff9119
Anita6x <eanitaokele@gmail.com>
2025-11-29 16:28:24 +0100

    Add initial superuser creation command

--- django_project/mediahub/mediahub/wsgi.py
+++ django_project/mediahub/mediahub/wsgi.py
@@ -14,3 +14,15 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mediahub.settings')
 
 application = get_wsgi_application()
+
+# in mediahub/wsgi.py at the bottom
+
+import django
+django.setup()
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser("admin", "eanitaokele@gmail.com", "testuser123")
+
